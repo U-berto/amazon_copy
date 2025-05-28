7e4c9e9a2d9bcf48e309d40e1777dd2d8404e35a
Umberto Sgariglia <us.sgariglia@yahoo.it>
2025-05-28 16:43:46 +0200

    unfinished orders-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:show, :index]
   resources :cart_items, only: [:create]
+  resources :orders, only: [:index]
   root to: "items#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
