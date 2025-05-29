8ec666c3214d1e077a58c87fbaa20dc671504b39
Aboudi youssef <aboudiyoussef222@gmail.com>
2025-05-29 15:57:28 +0200

    sign in and out features

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :order_items, through: :orders
   after_create :generate_cart
 
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def generate_cart
     Cart.create!(user: self)
   end
